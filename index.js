363a3899f0c2d1a03073451e73d787c2e92610d1
zaingova <zain.gova.school@gmail.com>
2023-05-30 15:13:42 -0400

    changed the inquirer prompt so that the user can pick any colour they want, whether from a list of predefined HTML colors or a hexadecimal value

--- index.js
+++ index.js
@@ -2,70 +2,142 @@
 const { Circle, Triangle, Rectangle } = require('./lib/dev/shape')
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { validate } = require('@babel/types');
 
-// renderShape function - based on the user-choice for the shape, creates a class-instance and calls it's unique render function
-renderShape = ((data) => {
-    switch (data.shapeType) {
-        case "Circle":
-            const circle = new Circle((data.shapeColor).toLowerCase(), (data.textColor).toLowerCase(), data.text);
-            console.log(circle.render());
-            break;
-        case "Triangle":
-            const triangle = new Triangle((data.shapeColor).toLowerCase(), (data.textColor).toLowerCase(), data.text);
-            console.log(triangle.render());
-            break;
-        case "Rectangle":
-            const rect = new Rectangle((data.shapeColor).toLowerCase(), (data.textColor.toLowerCase()), data.text);
-            console.log(rect.render());
-            break;
-    }
-})
+class SVGLogo {
 
-getInput = (() => {
-    inquirer.
-        prompt([
-            {
-                type: "list",
-                message: "Please enter a shape:",
-                name: "shapeType",
-                choices: ["Circle", "Triangle", "Rectangle"]
-            },
-            {
-                type: "input",
-                message: "Enter a logo-text (3 CHARACTERS-MAX)",
-                name: "text",
+    // renderShape function - based on the user-choice for the shape, creates a class-instance and calls it's unique render function
+    renderShape = ((data) => {
+        switch (data.shapeType) {
+            case "Circle":
+                const circle = new Circle((data.shapeColor).toLowerCase(), (data.textColor).toLowerCase(), data.text);
+                return (circle.render());
+            case "Triangle":
+                const triangle = new Triangle((data.shapeColor).toLowerCase(), (data.textColor).toLowerCase(), data.text);
+                return (triangle.render());
+            case "Square":
+                const rect = new Rectangle((data.shapeColor).toLowerCase(), (data.textColor.toLowerCase()), data.text);
+                return (rect.render());
+        }
+    })
 
-                // validation to ensure the text is of valid length
-                validate(text) {
-                    if (text.length > 3 || text.length == 0) {
-                        return "INVALID LENGTH";
+    // creates new file called "logo.svg" and writes the data from the shape render function
+    createSVG = ((data) => {
+        fs.writeFile("logo.svg", data, (err) => {
+            (err) ? console.log(err) : console.log("Generated logo.svg");
+        })
+    })
+
+    // takes user through a set of questions which define their logo
+    getInput = ((svgLogo) => {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    message: "Please enter a shape:",
+                    name: "shapeType",
+                    choices: ["Circle", "Triangle", "Square"]
+                },
+                {
+                    type: "input",
+                    message: "Enter a logo-text (3 CHARACTERS-MAX)",
+                    name: "text",
+
+                    // validation to ensure the text is of valid length
+                    validate(text) {
+                        if (text.length > 3 || text.length == 0) {
+                            throw new Error("INVALID LENGTH");
+                        }
+                        return true;
                     }
+                },
+                {
+                    type: "input",
+                    message: "Please enter a color for the shape (color name or 6-digit HEX code [include the '#']):",
+                    name: "shapeColor",
+
+                    validate: function (shapeColor) {
+                        if (svgLogo.validateColor(shapeColor)) {
+                            return true;
+                        } else {
+                            throw new Error("INVALID COLOR");
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    message: "Please enter a color for the text (color name or 6-digit HEX code [include the '#']):",
+                    name: "textColor",
+
+                    validate: function (textColor) {
+                        if (svgLogo.validateColor(textColor)) {
+                            //console.log(svgLogo.validateColor(this.textColor))
+                            return true;
+                        } else {
+                            throw new Error("INVALID COLOR");
+                        }
+                    }
+
+                }
+            ])
+
+            // once data is retrieved, calls renderShape function
+            .then((data) => {
+                // pass the return value from renderShape into the createSVG function - renderShape returns SVG data
+                this.createSVG(this.renderShape(data));
+                return data;
+            })
+    })
+
+    // initializer function - starts the application with getting user-input for the logo
+    init = (() => {
+        this.getInput(this);
+    })
+
+    // validate color
+    validateColor = ((color) => {
+        const regEx = /^#[0-9A-F]{6}$/i;
+        const validColors = [
+            "aqua",
+            "black",
+            "blue",
+            "fuchsia",
+            "gray",
+            "green",
+            "lime",
+            "maroon",
+            "navy",
+            "olive",
+            "purple",
+            "red",
+            "silver",
+            "teal",
+            "white",
+            "yellow",
+            "orange"
+        ]
+
+        // if color is a valid hexadecimal value
+        if (regEx.test(color)) {
+            // return true
+            return true;
+        } else {
+            // otherwise iterate through all valid colors and compare given color to each value
+            for (let i = 0; i < validColors.length; i++) {
+                // if there is a match, return true
+                if (color == validColors[i]) {
                     return true;
                 }
-            },
-            {
-                type: "list",
-                message: "Please enter a color for the shape:",
-                name: "shapeColor",
-                choices: ["Blue", "Red", "Green", "Yellow", "Orange", "White", "Purple", "Cyan"]
-            },
-            {
-                type: "list",
-                message: "Please enter a color for the text:",
-                name: "textColor",
-                choices: ["Blue", "Red", "Green", "Yellow", "Orange", "White", "Purple", "Cyan"]
             }
-        ])
+        }
 
-        // once data is retrieved, calls renderShape function
-        .then((data) => {
-            renderShape(data);
-        })
-})
+        // if the function has not returned true by this point, the color cannot be valid
+        return false;
+    })
+}
 
-// initializer function - starts the application with getting user-input for the logo
-init = (() => {
-    getInput();
-})
+// creates class instance
+const svgLogo = new SVGLogo();
 
-init();
+// calls the init function to run the app
+svgLogo.init();
